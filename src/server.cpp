c668a9ed229ae93081878098a3a1a4b3c6161391
sardylan <sardylan@gmail.com>
2018-06-10 15:50:22 +0200

    Adds license, readme and first files

---
+++ src/server.cpp
@@ -0,0 +1,54 @@
+/*
+ * SHAB Server
+ * Copyright (C) 2018  Luca Cireddu <sardylan@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <iostream>
+#include <QtCore/QTextStream>
+
+#include "server.hpp"
+#include "version.hpp"
+
+using namespace org::thehellnet::shab;
+
+int main(int argc, char **argv) {
+    QCoreApplication::setApplicationName(SHAB_SERVER_APPLICATION_NAME);
+    QCoreApplication::setApplicationVersion(SHAB_SERVER_APPLICATION_VERSION);
+    QCoreApplication::setOrganizationName(SHAB_SERVER_ORGANIZATION_NAME);
+    QCoreApplication::setOrganizationDomain(SHAB_SERVER_ORGANIZATION_DOMAIN);
+
+    ShabServer shabServer(argc, argv);
+    shabServer.run();
+    return shabServer.exec();
+}
+
+ShabServer::ShabServer(int &argc, char **argv) : QCoreApplication(argc, argv) {
+
+}
+
+ShabServer::~ShabServer() = default;
+
+void ShabServer::run() {
+    printHeader();
+
+    QMetaObject::invokeMethod(this, "quit", Qt::QueuedConnection);
+}
+
+void ShabServer::printHeader() {
+    QTextStream stdErr(stderr);
+    stdErr << applicationName() << " " << applicationVersion() << endl;
+    stdErr << "Running with PID " << applicationPid() << endl;
+}
---
+++ src/server.hpp
@@ -0,0 +1,43 @@
+/*
+ * SHAB Server
+ * Copyright (C) 2018  Luca Cireddu <sardylan@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef __SHAB_SERVER_SERVER_H
+#define __SHAB_SERVER_SERVER_H
+
+#include <QtCore/QCoreApplication>
+
+namespace org::thehellnet::shab {
+
+    class ShabServer : public QCoreApplication {
+    Q_OBJECT
+
+    public:
+
+        explicit ShabServer(int &argc, char **argv);
+
+        ~ShabServer() override;
+
+        void run();
+
+    private:
+
+        void printHeader();
+    };
+};
+
+#endif
